8cb6cdf80b254ea570b4f2985cdcd11442579755
TOB-CYXOB <tovSuhov@yandex.ru>
2013-09-08 15:43:44 +0600

    lang EN 004

--- inc/lang/en.php
+++ inc/lang/en.php
@@ -6,7 +6,7 @@
 	$LANG['SITENAME_NOPLACE']  = "No place";
 	$LANG['TRANSPORT_PLACES_COUNT_ARR'] = array('place','place','places');
 	$LANG["TRIP_CREATE_TITLE"] = "Создать новый Трип";
-	$LANG['TRIP_MYTRIPS_TITLE'] = "Мои трипы";
+	$LANG['TRIP_MYTRIPS_TITLE'] = "My trips";
 	$LANG['TRIP_LASTTRIPS_TITLE'] = "Последние трипы";
 	$LANG["USER_REGEMAIL_SUBJECT"] = 'Экспресс-регистрация на сайте EstMesto.Net';
 	$LANG["USER_REGEMAIL_DONE"] = 'Экспресс-регистрация успешно завершена. Ключ-ссылка для входа выслана на ваш адрес.';
